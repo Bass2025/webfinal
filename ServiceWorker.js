1467bdf63cf24fe11eb6f2d31dbf22c426cf455c
Bass2025 <zzzzzz66833@gmail.com>
2024-08-03 13:00:41 +0800

    0803NewUpdate

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-HuangZiHeng-0.1";
+const contentToCache = [
+    "Build/0803NewHuangZIWEB.loader.js",
+    "Build/0803NewHuangZIWEB.framework.js",
+    "Build/0803NewHuangZIWEB.data",
+    "Build/0803NewHuangZIWEB.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
